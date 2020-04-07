acd2b479e7edb172fa2e6fc3a2b296026a980913
Basile Simon <basile.simon@thomsonreuters.com>
2020-04-07 13:46:36 +0200

    Parse CSV to JSON, reformat structure

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const fs = require('fs');
 const path = require('path');
 const csv = require('csvtojson');
 const axios = require('axios');
+const d3 = Object.assign({}, require('d3-collection'));
 
 const headline_figures =
-  'https://raw.githubusercontent.com/datasciencecampus/google-mobility-reports-data/master/csvs/uk-international-percentage-falls.csv';
+  'https://raw.githubusercontent.com/datasciencecampus/google-mobility-reports-data/master/csvs/international_national_trends_G20_SE.csv';
 const SOURCE_PATH_HEADLINES = path.resolve(
   __dirname,
   'data/headline_figures.csv'
@@ -27,17 +28,57 @@ async function downloadLatest(file, to_location) {
   });
 }
 
-const parseCSVToJSON = (path) =>
-  csv()
-    .fromFile(path)
-    .then((jsonObj) => {
-      console.log(jsonObj);
-    });
+async function parseCSVToJSON(source_path) {
+  const parsedCSV = await csv()
+    .fromFile(source_path)
+    .then((jsonObj) => jsonObj);
+
+  const uniqueDates = Object.keys(parsedCSV[0]).splice(
+    3,
+    Object.keys(parsedCSV[0]).length - 1
+  );
+
+  const data = { series: uniqueDates, data: {} };
+
+  const groupByCountry = d3
+    .nest()
+    .key((d) => d.Country)
+    .key((d) => d.Category)
+    .entries(parsedCSV);
+
+  for (let i = 0; i < groupByCountry.length; i++) {
+    const country = groupByCountry[i].key;
+    const countryData = groupByCountry[i].values;
+    data.data[country] = [];
+
+    for (let z = 0; z < countryData.length; z++) {
+      const category = countryData[z].key;
+      const categoryData = countryData[z].values;
+
+      const simpleData = data.series.map((e) => {
+        const match = categoryData[0][e];
+        if (match) {
+          return parseInt(match);
+        }
+      });
+
+      const categoryAndData = {};
+      categoryAndData[category] = simpleData;
+
+      data.data[country].push(categoryAndData);
+    }
+  }
+  writeJSONLocally(data, path.resolve(__dirname, 'data/latest_parsed.json'));
+  console.log(data.data);
+}
+
+const writeJSONLocally = (data, location) =>
+  fs.writeFileSync(location, JSON.stringify(data, 4));
 
 const run = async () => {
   try {
     await downloadLatest(headline_figures, SOURCE_PATH_HEADLINES);
-    parseCSVToJSON(SOURCE_PATH_HEADLINES);
+    await parseCSVToJSON(SOURCE_PATH_HEADLINES);
   } catch (err) {
     throw err;
   }
